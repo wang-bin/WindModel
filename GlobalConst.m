ea0d4adff5158e47dffedba335ec217ba416fbc5
wang-bin <wbsecg1@gmail.com>
2012-09-26 11:30:26 +0800

    use load instead of importdata to compat with freemat

--- GlobalConst.m
+++ GlobalConst.m
@@ -34,33 +34,38 @@ global Pressure;
 
 %importdata: get struct. 
 %load: load var
-aa=importdata('IR1_2030.mat');
-bb=importdata('IR1_2100.mat');
-cc=importdata('IR1_2130.mat');
-Data = importdata('Data.mat');
-TestP= Data.TestP; %importdata('TestP.mat');
-TestPRad=deg2rad(TestP);
-cl =Data.cl; %importdata('cl.mat');
+%aa=importdata('IR1_2030.mat');
+%bb=importdata('IR1_2100.mat');
+%cc=importdata('IR1_2130.mat');
+%Data = importdata('Data.mat');
+%TestP= Data.TestP; %importdata('TestP.mat');
+%cl =Data.cl; %importdata('cl.mat');
+%KT = Data.KT; 
+%Pressure = Data.Pressure; %importdata('Pressure.mat');
 %LLTP = importdata('temp3.mat');
-KT = Data.KT; 
+load('IR1_2030.mat');
+load('IR1_2100.mat');
+load('IR1_2130.mat');
+load('Data.mat');
+TestPRad=deg2rad(TestP);
+
 %{ 
-importdata('k_temp.txt', ' ', 3);
-KT = KT.data';
-KT = KT(:);
+%importdata('k_temp.txt', ' ', 3);
+%KT = KT.data';
+%KT = KT(:);
 %}
-Pressure = Data.Pressure; %importdata('Pressure.mat');
 
 Earth_a = 6378136.5;
 Earth_b = 6356751.8;
-Earth_e = sqrt(1-(Earth.b/Earth.a)^2);
+Earth_e = sqrt(1-(Earth_b/Earth_a)^2);
 Earth_R = (Earth_a+Earth_b)/2;
 Const_SatH = 42164000;
-Const_Ha = Const.SatH/Earth.a;
-Const_Hb = Const.SatH/Earth.b;
-Const_ab = Earth.a/Earth.b;
-Const_ab2 = Const.ab^2;
-Const_ba = Earth.b/Earth.a;
-Const_ba2 = Const.ba^2;
+Const_Ha = Const_SatH/Earth_a;
+Const_Hb = Const_SatH/Earth_b;
+Const_ab = Earth_a/Earth_b;
+Const_ab2 = Const_ab^2;
+Const_ba = Earth_b/Earth_a;
+Const_ba2 = Const_ba^2;
 Const_delta = 0.00014;
 Const_Row0 = 1145;
 Const_Col0 = 1145;
